724ed278b20c64c73b296345ab399fd594848fd4
Braun Goodson <braungoodson@gmail.com>
2013-10-13 20:47:59 -0400

    ok

--- deathdungeon.js
+++ deathdungeon.js
@@ -1,16 +1,3 @@
-var clc = require('cli-color')
-var error = clc.red.bold;
-var warn = clc.yellow;
-var notice = clc.cyan;
-var deathdungeon = clc.red;
-var say = clc.green;
-
-console.log(deathdungeon('deathdungeon:')+' dungeon is open');
-
-var fs = require('fs');
-
-var players = [];
-
 var mario = require('mario-mario');
 var plumbing = require('deathdungeon-plumbing');
 mario.plumbing(plumbing);
